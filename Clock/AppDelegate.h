f43858d3f22ef4b2e3d67591ab9e150ac3cc8465
非典型技术宅 <3840553@qq.com>
2017-02-03 22:53:12 +0800

    Upload Clock/AppDelegate.h Clock/Info.plist Clock/AppDelegate.m Clock/main.m Clock/ViewController.m Clock.xcodeproj/project.pbxproj Clock/ViewController.h Clock/Base.lproj/LaunchScreen.storyboard Clock.xcodeproj/project.xcworkspace/contents.xcworkspacedata Clock/Base.lproj/Main.storyboard Clock/Assets.xcassets/AppIcon.appiconset/Contents.json Clock.xcodeproj/project.xcworkspace/xcuserdata/stan.xcuserdatad/UserInterfaceState.xcuserstate Clock.xcodeproj/xcuserdata/stan.xcuserdatad/xcschemes/Clock.xcscheme Clock.xcodeproj/xcuserdata/stan.xcuserdatad/xcschemes/xcschememanagement.plist

---
+++ Clock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clock
+//
+//  Created by Stan on 2017-02-03.
+//  Copyright © 2017 stan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by Stan on 2017-02-03.
+//  Copyright © 2017 stan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
